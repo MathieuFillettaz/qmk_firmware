1d6c885533b0f0e7862d61c32135d97c3804acc2
MathieuFillettaz <97092288+MathieuFillettaz@users.noreply.github.com>
2023-04-24 20:09:24 +0200

    Fixing space cadet shift on Cantor

--- keyboards/cantor/keymaps/custom/keymap.c
+++ keyboards/cantor/keymaps/custom/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
         KC_TAB,            KC_Q, KC_W, KC_F,    KC_P,  KC_G,                          KC_J,             KC_L,  KC_U,    KC_Y,   KC_QUOT, KC_BSPC,
         LCTL_T(KC_ESCAPE), KC_A, KC_R, KC_S,    KC_T,  KC_D,                          KC_H,             KC_N,  KC_E,    KC_I,   KC_O,    KC_SCLN,
-        KC_LSPO,           KC_Z, KC_X, KC_C,    KC_V,  KC_B,                          KC_K,             KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
+        SC_LSPO,           KC_Z, KC_X, KC_C,    KC_V,  KC_B,                          KC_K,             KC_M,  KC_COMM, KC_DOT, KC_SLSH, SC_RSPC,
                                        KC_LGUI, LT(1, KC_DEL), LT(2,KC_ENTER),        LALT_T(KC_SPACE), LT(3,KC_BSPC), KC_RALT
     ),
     //FUNCANDKEYPAD
